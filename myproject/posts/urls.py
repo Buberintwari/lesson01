33cb056c99062259beed293e3cba5b66c9c9d970
Buberintwari <emerypatience@gmail.com>
2024-07-19 15:17:41 +0200

    last07192024

--- myproject/posts/urls.py
+++ myproject/posts/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path # type: ignore
+from django.urls import path
 from . import views
 
-#app_name ='posts'
+app_name = 'posts'
 
 urlpatterns = [
-    path('', views.posts_list, name="posts"),
-    path('<slug:slug>', views.post_page, name="post_page"),
+    path('', views.posts_list, name="list"),
+    path('new-post/', views.post_new, name="new-post"),
+    path('<slug:slug>/', views.post_page, name="page"),
 ]
